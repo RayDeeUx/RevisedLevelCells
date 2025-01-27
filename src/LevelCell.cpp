6996fdb14155d8ed41d4fecf5260a1d9337c72e0
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-01-26 20:06:08 -0500

    life.

--- src/LevelCell.cpp
+++ src/LevelCell.cpp
@@ -223,7 +223,7 @@ class $modify(MyLevelCell, LevelCell) {
 	void loadLocalLevelCell() {
 		LevelCell::loadLocalLevelCell();
 		if (!(Utils::modEnabled() && Utils::getBool("compactEditorLevels"))) return;
-		if (CCNode* localLevelName = typeinfo_cast<CCLabelBMFont*>(getChildByIDRecursive("level-name"))) localLevelName->limitLabelWidth(200.f, .6f, .01f);
+		if (CCLabelBMFont* localLevelName = typeinfo_cast<CCLabelBMFont*>(getChildByIDRecursive("level-name"))) localLevelName->limitLabelWidth(200.f, .6f, .01f);
 		if (CCNode* mainLayer = getChildByIDRecursive("main-layer")) mainLayer->setPositionY(-3.f);
 	}
 	void loadCustomLevelCell() {
