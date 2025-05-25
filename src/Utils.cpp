00378104f610852cea6d27b3b7785171804dbc4b
Erymanthus (RayDeeUx) <51521765+RayDeeUx@users.noreply.github.com>
2025-05-25 12:53:18 -0400

    added a few more logs

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -162,10 +162,12 @@ namespace Utils {
 		}
 		if (Utils::getBool("friendsAreFavoriteUsers")) {
 			glm->m_userListDelegate = simpson;
+			log::info("fetching UserListType::Friends");
 			glm->getUserList(UserListType::Friends);
 		}
 		if (Utils::getBool("blockedAreIgnoredPeople")) {
 			glm->m_userListDelegate = simpson;
+			log::info("fetching UserListType::Blocked");
 			glm->getUserList(UserListType::Blocked);
 		}
 		return true;
